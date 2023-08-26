69404e8acac6d801fb38f5edcb5ab309c0e912bc
re1vl <re1vlmail@gmail.com>
2023-08-26 16:09:45 +0300

    responsive design

--- src/components/List/ListContainer.jsx
+++ src/components/List/ListContainer.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import StateComponent from "../State/StateComponent";
 import List from "./List";
+import s from "./List.module.css";
 
 const ListContainer = () => {
   const state = useContext(StateComponent);
   return (
-    <ul>
+    <ul className={s.ul}>
       {Object.values(state.getCards()).map((item) => (
         <List item={item} key={item.name} />
       ))}
--- src/components/PasswordGenerator/PasswordGeneratorLogic/PasswordGeneratorLogic.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PasswordGeneratorLogic = (props) => {
-  return <div></div>;
-};
-
-export default PasswordGeneratorLogic;
